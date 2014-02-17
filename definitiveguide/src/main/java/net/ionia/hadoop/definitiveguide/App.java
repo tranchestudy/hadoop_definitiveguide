76870fbe139bf5d2d0fca9f84cad9c02c047dcd5
Sung Woo Kim <ionia.kim@gmail.com>
2014-02-17 22:25:44 +0900

    initial

---
+++ definitiveguide/src/main/java/net/ionia/hadoop/definitiveguide/App.java
@@ -0,0 +1,13 @@
+package net.ionia.hadoop.definitiveguide;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
